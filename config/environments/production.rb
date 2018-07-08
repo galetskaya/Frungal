3f73f5ac0d0eb835175c836f55ebf0ab4b784804
alisafrunza <alisafrunza@gmail.com>
2018-07-08 14:54:25 +0300

    store locally

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.require_master_key = true
 
   # Store files on Amazon S3.
-  config.active_storage.service = :cloudinary
+  config.active_storage.service = :local
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
