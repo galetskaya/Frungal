5adb2c99eda8f270598f8e9b095d8ac9cff3b00e
alisafrunza <alisafrunza@gmail.com>
2018-07-27 14:42:10 +0300

    remove album not null constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_08_100101) do
+ActiveRecord::Schema.define(version: 2018_07_27_114052) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,8 +53,8 @@ ActiveRecord::Schema.define(version: 2018_07_08_100101) do
   create_table "albums", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
