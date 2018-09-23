ff9b60855c82a2e234bb999ecbb4b541fcea8a34
Alika <li-gn@yandex.ru>
2018-09-23 19:50:37 +0300

    Modify landing page

--- app/assets/javascripts/portfolio/main.js
+++ app/assets/javascripts/portfolio/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Julia Galetskaya.", "Photographer.", "Julia Galetskaya.", "Photographer.",  "Awesome :)"],
       typeSpeed: 100,
       loop: true,
     });
